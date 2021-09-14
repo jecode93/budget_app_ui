51081fba00f8193aced7b70698526e4b41a1f595
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-09-13 22:23:28 -0400

    Cleaning the app and add folders for next files

---
+++ lib/screens/home_screens.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
