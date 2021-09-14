1f0d4a1d3cd77681baa719282c4b0592efa2a37a
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-09-13 22:39:00 -0400

    Sliver App Bar and Sliver List, prepare the HomeScreen

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -12,6 +12,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            forceElevated: true,
+            expandedHeight: 100.0,
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget UI'),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+              ),
+            ],
+          ),
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.all(10),
+                color: Colors.blue,
+                height: 100.0,
+              );
+            }),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+
+// appBar: AppBar(
+//         leading: IconButton(
+//           icon: Icon(Icons.settings),
+//           onPressed: () {},
+//         ),
+//         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+//       ),
