c363a8860255f12e02df9039a8404725a5cf4ca4
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-09-14 23:11:35 -0400

    Category List Cards

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,4 +1,6 @@
 import 'package:budger_app_ui/data/data.dart';
+import 'package:budger_app_ui/models/category_model.dart';
+import 'package:budger_app_ui/models/expense_model.dart';
 import 'package:budger_app_ui/widgets/bar_chart.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  _buildCategory(Category category, double totalAmountSpent) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      height: 100.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, 2), color: Colors.black12, blurRadius: 6.0),
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  category.name.toString(),
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '\$${(category.maxAmount! - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount!.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,34 +82,35 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverList(
             delegate:
                 SliverChildBuilderDelegate((BuildContext context, int index) {
-              return Container(
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset(0, 2),
-                      blurRadius: 6.0,
-                    )
-                  ],
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: BarChar(expenses: weeklySpending),
-              );
-            }, childCount: 1),
+              if (index == 0) {
+                return Container(
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0, 2),
+                        blurRadius: 6.0,
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: BarChar(expenses: weeklySpending),
+                );
+              } else {
+                final Category category = categories[index - 1];
+                double totalAmountSpent = 0;
+
+                category.expenses!.forEach((Expense expense) {
+                  totalAmountSpent += expense.cost!;
+                });
+                return _buildCategory(category, totalAmountSpent);
+              }
+            }, childCount: 1 + categories.length),
           ),
         ],
       ),
     );
   }
 }
-
-
-// appBar: AppBar(
-//         leading: IconButton(
-//           icon: Icon(Icons.settings),
-//           onPressed: () {},
-//         ),
-//         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
-//       ),
