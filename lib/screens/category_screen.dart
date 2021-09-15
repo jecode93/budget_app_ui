ac2633648ea78db1f2a7d5de409022473c510244
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-09-15 00:52:53 -0400

    Expense List

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:budger_app_ui/helpers/color_helper.dart';
 import 'package:budger_app_ui/models/category_model.dart';
 import 'package:budger_app_ui/models/expense_model.dart';
@@ -13,6 +15,57 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenseList = [];
+    widget.category!.expenses!.forEach((Expense expense) {
+      expenseList.add(Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 10.0,
+        ),
+        height: 80.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 2),
+              color: Colors.black12,
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                expense.name!,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '\$${expense.cost!.toStringAsFixed(2)}',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.red),
+              ),
+            ],
+          ),
+        ),
+      ));
+    });
+    return Column(
+      children: expenseList,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double containerMarginAndPadding = 20.0;
@@ -48,7 +101,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   boxShadow: [
                     BoxShadow(
                       offset: Offset(0, 2),
-                      color: Colors.grey,
+                      color: Colors.black12,
                       blurRadius: 6.0,
                     ),
                   ]),
@@ -70,6 +123,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
                 ),
               ),
             ),
+            _buildExpenses(),
           ],
         ),
       ),
